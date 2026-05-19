1254d2c754ce03a48354e89c707322bb280237f5
Jay <gabergambler@gmail.com>
2026-05-19 11:55:32 +0900

    수업분 5/19

--- Assets/10. Scripts/1. Unity/05. Layer And Scroll/ScrollController.cs
+++ Assets/10. Scripts/1. Unity/05. Layer And Scroll/ScrollController.cs
@@ -18,7 +18,12 @@ namespace Study.LayerAndScroll
         public Transform endPivot;
         public Transform spawnPivot;
 
-        public List<GameObject> enableLayerList = new List<GameObject>();
+        private List<GameObject> enableLayerList = new List<GameObject>();
+
+        private void Start()
+        {
+            enableLayerList.Add(startLayer);
+        }
 
         private void Update()
         {
@@ -30,6 +35,37 @@ namespace Study.LayerAndScroll
             {
                 enableLayerList[i].transform.Translate(moveVector);
             }
+
+            // 2. 가장 첫번째 Layer(enableLayerList[0])가
+            // EndPivot의 경계를 넘어간다면(x값보다 작아진다면)
+            // 삭제한다.
+
+            GameObject headLayer = enableLayerList[0]; 
+            // 가장 앞에있는 Layer오브젝트를 가져옵니다
+
+            if(headLayer.transform.position.x <= endPivot.position.x)
+            {
+                enableLayerList.RemoveAt(0);
+                Destroy(headLayer);
+            }
+
+            // 3. Layer를 생성해줍니다. 현재 필요한 LayerObject 2 ~ 3개입니다
+            // GameObject.Instantiate(GameObject object) || .Instantiate(GameObject object)
+            // 런타임 중에 매개변수로 들어온 object의 사본을 생성합니다.
+
+            // .Instantiate() : 실행중(런타임)에 게임오브젝트를 생성하는 함수 입니다.
+            //                  생성한 객체는 생성할 객체의 타입으로 반환받을 수 있습니다 
+            
+            while(enableLayerList.Count <= 2)
+            {
+                GameObject instance = Instantiate(layerPrefabs[0], // layerPrefabs[0]개체의 사본을 전달합니다
+                    spawnPivot.transform.position, spawnPivot.rotation);
+                    // spawnPivot의 위치, spawnPivot의 회전값 이라는 말.    
+                enableLayerList.Add(instance);
+            }
+            
+
+            
 
         }
     }
