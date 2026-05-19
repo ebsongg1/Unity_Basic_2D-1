0983ace935d933f1ca943210c5cfad09617fb0b6
Jay <gabergambler@gmail.com>
2026-05-19 12:55:12 +0900

    UFO 게임 세팅

--- Assets/10. Scripts/1. Unity/05. Layer And Scroll/ScrollController.cs
+++ Assets/10. Scripts/1. Unity/05. Layer And Scroll/ScrollController.cs
@@ -7,8 +7,11 @@ namespace Study.LayerAndScroll
     // Layer의 속도를 조절해 볼겁니다
     public class ScrollController : MonoBehaviour
     {
+        public enum ScrollDirection { Left, Right, Up, Down }
+
         [Header("Scroll Settings")]
         public float speed = 1.0f;
+        public ScrollDirection direction = ScrollDirection.Left;
 
         [Header("Resources")]
         public GameObject[] layerPrefabs;
@@ -27,46 +30,94 @@ namespace Study.LayerAndScroll
 
         private void Update()
         {
+            MoveLayerList();
+            CheckDestroyAbleLayer();
+            CheckInstantiateLayer();
+        }
+
+        private Vector3 GetMoveDirection(ScrollDirection dir)
+        {
+            switch(dir)
+            {
+                case ScrollDirection.Left:
+                    return Vector3.left;
+                case ScrollDirection.Right:
+                    return Vector3.right;
+                case ScrollDirection.Up:
+                    return Vector3.up;
+                case ScrollDirection.Down:
+                    return Vector3.down;
+                default:
+                    return Vector3.left;
+            }
+        }
+
+        private void MoveLayerList()
+        {
             // (speed * Time.deltaTime) = 초당 speed의 속도로 뭔가를 하겠다는 표현
-            Vector3 moveVector = Vector3.left * (speed * Time.deltaTime);
+            Vector3 dir = GetMoveDirection(direction);
+            Vector3 moveVector = dir * (speed * Time.deltaTime);
 
             // 1. 활성화된 모든 레이어를 moveVector만큼 옮겨준다
-            for(int i = 0; i < enableLayerList.Count; ++i)
+            for (int i = 0; i < enableLayerList.Count; ++i)
             {
                 enableLayerList[i].transform.Translate(moveVector);
             }
+        }
 
+        private void CheckDestroyAbleLayer()
+        {
             // 2. 가장 첫번째 Layer(enableLayerList[0])가
             // EndPivot의 경계를 넘어간다면(x값보다 작아진다면)
             // 삭제한다.
 
-            GameObject headLayer = enableLayerList[0]; 
+            GameObject headLayer = enableLayerList[0];
             // 가장 앞에있는 Layer오브젝트를 가져옵니다
 
-            if(headLayer.transform.position.x <= endPivot.position.x)
+            bool check = false;
+            
+            switch(direction)
+            {
+                case ScrollDirection.Left:
+                    // headLayer의 x가 endPivot보다 작다면
+                    check = headLayer.transform.position.x <= endPivot.position.x;
+                    break;
+                case ScrollDirection.Right:
+                    // headLayer의 x가 endPivot보다 크다면
+                    check = headLayer.transform.position.x >= endPivot.position.x;
+                    break;
+                case ScrollDirection.Up:
+                    // headLayer의 y가 endPivot보다 크다면
+                    check = headLayer.transform.position.y >= endPivot.position.y;
+                    break;
+                case ScrollDirection.Down:
+                    // headLayer의 y가 endPivot보다 작다면
+                    check = headLayer.transform.position.y <= endPivot.position.y;
+                    break;
+            }
+
+            // 왼쪽케이스
+            if (check)
             {
                 enableLayerList.RemoveAt(0);
                 Destroy(headLayer);
             }
+        }
 
+        private void CheckInstantiateLayer()
+        {
             // 3. Layer를 생성해줍니다. 현재 필요한 LayerObject 2 ~ 3개입니다
-            // GameObject.Instantiate(GameObject object) || .Instantiate(GameObject object)
-            // 런타임 중에 매개변수로 들어온 object의 사본을 생성합니다.
 
-            // .Instantiate() : 실행중(런타임)에 게임오브젝트를 생성하는 함수 입니다.
-            //                  생성한 객체는 생성할 객체의 타입으로 반환받을 수 있습니다 
-            
-            while(enableLayerList.Count <= 2)
+            //  GameObject.Instantiate(GameObject object) || .Instantiate(GameObject object)
+            // 실행중(런타임)에 매개변수로 들어온 object의 사본을 생성합니다.
+            // 생성한 객체는 생성할 객체의 타입으로 반환받을 수 있습니다                  
+            while (enableLayerList.Count <= 2)
             {
                 GameObject instance = Instantiate(layerPrefabs[0], // layerPrefabs[0]개체의 사본을 전달합니다
                     spawnPivot.transform.position, spawnPivot.rotation);
-                    // spawnPivot의 위치, spawnPivot의 회전값 이라는 말.    
+                // spawnPivot의 위치, spawnPivot의 회전값 이라는 말.    
                 enableLayerList.Add(instance);
             }
-            
-
-            
-
         }
     }
 }
---
+++ Assets/10. Scripts/1. Unity/06. UFO Game/UFOController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UFOController : MonoBehaviour
+{
+    // 위,아래 움직임 기능 넣어보기
+    // 기본적으로 하강을 하지만 스페이스를 눌렀을때는 상승
+
+    public float Speed = 1.0f;
+    public int CoinCount = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //기본적으로 하강
+        Vector3 moveVector = new Vector3(0, -1, 0);
+
+        // Space를 누르면 상승한다
+        if (Keyboard.current.spaceKey.isPressed)
+        {
+            moveVector = moveVector * (-1f); // -1 * -1 = +1;
+        }
+
+        //속도 * 프레임당 이동량을 구해준다
+        moveVector = moveVector * Speed * Time.deltaTime;
+
+        transform.position += moveVector;
+    }
+
+    // 이거 나중
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+        CoinCount += 1;
+    }
+}
